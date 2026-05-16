2b97161c68e390a02dc1b56a6ad13902e72a7710
Vijaivasan <vijaivasan2006@gmail.com>
2026-05-16 08:17:15 +0530

    Initialize Spring Boot Project with MySQL configuration

---
+++ fintrack-api/src/main/java/com/fintrack/api/FintrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.fintrack.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintrackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintrack/api/FintrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
